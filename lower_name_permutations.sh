0427934d08af027e2cb834b5ef2e30f06a75ee91
g0tie <56622131+g0tie@users.noreply.github.com>
2024-04-24 21:05:05 +0400

    Update lower_name_permutations.sh
    add first initial concatenation with second field seperated with a dot

--- lower_name_permutations.sh
+++ lower_name_permutations.sh
@@ -12,8 +12,13 @@ fi
 
 output_file="${1%.txt}_wordlist.txt" 
 
+# replace space with a dot and put everything in lowercase
 sed  -e 's/ /./g' -e 's/[[:upper:]]/\L&/g' "$1" > "$output_file"
+
+# reverse firstname and lastname, seprate with a dot and all in lowercase
 awk '{print tolower($2 "." $1)}' "$1" >> "$output_file"
 
+# Keep first letter, put a dot and lowercase everything
+awk '{ first_letter = substr($1, 1, 1) #get first letter print tolower( substr($1, 1, 1) "." $2) }'
 
 echo "Operation Success, file at: $(pwd)/$output_file"
